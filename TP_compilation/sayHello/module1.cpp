3c458e3451d5e315e5831de48b8b9b93bfdd4a3f
Allan Doe <doe@nimpe.org>
2018-03-22 11:34:02 +0100

    Enregistrement

--- TP_compilation/sayHello/module1.cpp
+++ TP_compilation/sayHello/module1.cpp
@@ -3,7 +3,7 @@
 std::string getHello(int n) {
   //int * ptr = 0;
   //*ptr = 37;
-  std::string str = "Hello World ";
+  std::string str = "Hello World 3 ";
   str += std::to_string(n);
   str += "!";
   return str;
